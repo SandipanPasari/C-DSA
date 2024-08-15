7d3eebd4dcb01ac1e6902e89535d4196770c2873
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-08-15 23:36:23 +0530

    g

--- All Practice Sets/CQ.C
+++ All Practice Sets/CQ.C
@@ -51,3 +51,114 @@ int main()
     }
     return 0;
 }
+
+int isEmpty()
+{
+    if (front == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull()
+{
+    if ((front == 0 & rear == MAX - 1) || (front == rear + 1))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(int data)
+{
+    if (isFull())
+    {
+        printf("Queue Overflow\n");
+        exit(1);
+    }
+    if (front == -1)
+    {
+        front = 0;
+    }
+    if (rear == MAX - 1)
+    {
+        rear = 0;
+    }
+    else
+    {
+        rear = rear + 1;
+        cqueue[rear] = data;
+    }
+}
+
+int dequeue()
+{
+    int data;
+    if (isEmpty())
+    {
+        printf("Queue Underflow");
+        exit(1);
+    }
+    data = cqueue[front];
+    if (front == rear)
+    {
+        front = rear - 1;
+    }
+    else if (front == MAX - 1)
+    {
+        front = 0;
+    }
+    else
+    {
+        front = front + 1;
+    }
+    return data;
+}
+
+int peek()
+{
+    if (isEmpty())
+    {
+        printf("Queue Underflow");
+        exit(1);
+    }
+    return cqueue[front];
+}
+
+void print()
+{
+    int temp;
+    if (isEmpty())
+    {
+        printf("Queue underflow");
+        exit(1);
+    }
+    temp = front;
+    if (front <= rear)
+    {
+        printf("%d ", cqueue[temp]);
+        temp++;
+    }
+    else
+    {
+        while (temp <= MAX - 1)
+        {
+            printf("%d ", cqueue[temp]);
+            temp++;
+        }
+        temp = 0;
+        while (temp <= rear)
+        {
+            printf("%d ", cqueue[temp]);
+            temp++;
+        }
+    }
+    printf("\n");
+}
